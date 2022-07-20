b003d5f88901f7f6c5cc7f8660c55adcea073e29
Arthur Henrique Cavalcanti <50219237+Stuepp@users.noreply.github.com>
2022-07-19 23:40:41 -0300

    Update and rename index.js to styleme.css

--- index.js
+++
@@ -1,6 +0,0 @@
-function hoverAvatar(element){
-  element.setAttribute('src','https://c.tenor.com/ZgF7AEPZ4CwAAAAM/the-boys-real-heros-dont-do-drugs.gif')
-}
-function unhoverAvatar(element){
-  element.setAttribute('src','https://avatars.githubusercontent.com/u/50219237?v=4')
-}
